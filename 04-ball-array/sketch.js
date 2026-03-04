dd65463e82f07958f55083711064a325cb3892f8
mittpham <1203334@spsd.sk.ca>
2026-03-04 15:15:56 -0600

    Update sketch.js

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -4,6 +4,7 @@ let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
@@ -13,21 +14,38 @@ function draw() {
     ball.x += ball.dx;
     ball.y += ball.dy;
 
-    circle(ball.x, balll.y, ball.radius*2);
+    if (ball.x > width + ball.radius) {
+      ball.x = 0 + ball.radius;
+    }
+    else if (ball.x < 0 - ball.radius) {
+      ball.x = width - ball.radius;
+    }
+    else if (ball.y > height + ball.radius) {
+      ball.y = 0 + ball.radius;
+    }
+    else if (ball.y < 0 - ball.radius) {
+      ball.y = height - ball.radius;
+    }
+
+    fill(ball.r, ball.g, ball.b);
+    circle(ball.x, ball.y, ball.radius*2);
   }
 }
 
-function mousePresssed() {
-  spawnBall();
+function mousePressed() {
+  spawnBall(mouseX, mouseY);
 }
 
-function spawnBall() {
+function spawnBall(_x, _y) {
   let theBall = {
-    x: random(width),
-    y: random(height),
+    x: _x,
+    y: _y,
     dx: random(-5, 5),
     dy: random(-5, 5),
     radius: random(10, 40),
+    r: random(255),
+    g: random(255),
+    b: random(255),
   };
   ballArray.push(theBall);
 }
